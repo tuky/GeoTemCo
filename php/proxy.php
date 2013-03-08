964cfbba9ec94cbe6ca92e30e16ed506808fb1ca
Sebastian Kruse <skruse@mpiwg-berlin.mpg.de>
2013-03-08 12:17:41 +0100

    allow limiting of hosts for X-Domain download

--- php/proxy.php
+++ php/proxy.php
@@ -1,3 +1,15 @@
 <?php 
-	echo file_get_contents($_REQUEST['address']);
+	//Hosts that are allowed to download from. (e.g. "dropbox.com" and "www.dropbox.com")
+	//If this array is empty, all hosts are allowed.
+	$validHosts = array(
+			"localhost"
+	);
+
+	if (isset($_REQUEST['address'])){
+
+		$parsedAddress = parse_url($_REQUEST['address']);
+		
+		if ((count($validHosts)==0) || in_array($parsedAddress["host"], $validHosts))
+			echo file_get_contents($_REQUEST['address']);
+	}
 ?>
